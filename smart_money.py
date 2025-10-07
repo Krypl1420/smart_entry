70ab4e2df94042a8d788ad5b3dabe23ce4373517
jonas <zelene.okno@gmail.com>
2025-10-07 21:05:14 +0200

    .

--- smart_money.py
+++ smart_money.py
@@ -3,8 +3,12 @@ from discord_api import
 IB = ib_api.initialize_ib()
 def trade_range(range_high, range_low):
     while(True):
-        ib_api.get_last_n_bars(IB,)
-        candles = ib_api.
-        if current_price => range_high:
-
-        elif current
+        bars = ib_api.get_last_n_bars(IB,)
+        current_price = bars['close'].iloc[-1]
+        candles = ib_api.get_last_n_bars(IB, 'AAPL', 5, '1 min')
+        if current_price >= range_high:
+            pass
+            #sell function
+        elif current_price <= range_low:
+            pass
+            #buy function
